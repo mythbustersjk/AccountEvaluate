2149c25580076d30232c421a3950a21367d1df97
jack <bear93712kind>
2022-01-25 10:58:08 +0800

    fix some information cannot be read in win10

--- calculation.py
+++ calculation.py
@@ -21,7 +21,7 @@ def jc_calculation(jc_value):
             current_amount = input('请输入当前账户日均（万元）：')
             current_balance = input('请输入当前账户余额（万元）：')
             need_days = JcFormula(jc_value, current_amount, current_balance)
-            print(Fore.BLUE + f'该客户将在{round(need_days.jc_need_days_formula())}天后成为基础户')
+            print(Fore.YELLOW + f'该客户将在{round(need_days.jc_need_days_formula())}天后成为基础户')
             jc_tool = BreakOff.cls()
         elif jc == "2":
             total_days, end_date = days.day_js()
@@ -31,7 +31,7 @@ def jc_calculation(jc_value):
                 current_balance = 0
             need_found = JcFormula(jc_value, current_amount, current_balance, total_days)
             print(
-                Fore.BLUE + f'该客户需要存入{round(need_found.jc_amount_formula() - float(current_balance), 2)}万元，'
+                Fore.YELLOW + f'该客户需要存入{round(need_found.jc_amount_formula() - float(current_balance), 2)}万元，'
                             f'方可在{end_date}成为基础户')
             jc_tool = BreakOff.cls()
         elif jc == "3":
@@ -48,7 +48,7 @@ def yx_calculation(yx_value):
             current_amount = input('请输入当前账户日均（万元）：')
             current_balance = input('请输入当前账户余额（万元）：')
             need_days = YxFormula(yx_value, current_amount, current_balance)
-            print(Fore.BLUE + f'该客户将在{round(need_days.yx_need_days_formula())}天后成为基础户')
+            print(Fore.YELLOW + f'该客户将在{round(need_days.yx_need_days_formula())}天后成为基础户')
             yx_tool = BreakOff.cls()
         elif yx == "2":
             total_days, end_date = days.day_js()
@@ -58,7 +58,7 @@ def yx_calculation(yx_value):
                 current_balance = 0
             need_found = YxFormula(yx_value, current_amount, current_balance, total_days)
             print(
-                Fore.BLUE + f'该客户需要存入{round(need_found.yx_amount_formula() - float(current_balance), 2)}万元，'
+                Fore.YELLOW + f'该客户需要存入{round(need_found.yx_amount_formula() - float(current_balance), 2)}万元，'
                             f'方可在{end_date}成为有效户')
             yx_tool = BreakOff.cls()
         elif yx == "3":
