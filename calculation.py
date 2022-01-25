afe5136e46c5fba707716dee80bfb87565be3fff
jack <bear93712kind>
2022-01-25 11:17:34 +0800

    change code bcz formula module changed

--- calculation.py
+++ calculation.py
@@ -1,6 +1,6 @@
 import days
 
-from formula import JcFormula, YxFormula
+from formula import Formula
 
 import pg_list
 
@@ -20,8 +20,8 @@ def jc_calculation(jc_value):
         if jc == "1":
             current_amount = input('请输入当前账户日均（万元）：')
             current_balance = input('请输入当前账户余额（万元）：')
-            need_days = JcFormula(jc_value, current_amount, current_balance)
-            print(Fore.YELLOW + f'该客户将在{round(need_days.jc_need_days_formula())}天后成为基础户')
+            need_days = Formula(jc_value, current_amount, current_balance)
+            print(Fore.YELLOW + f'该客户将在{round(need_days.need_days_formula())}天后成为基础户')
             jc_tool = BreakOff.cls()
         elif jc == "2":
             total_days, end_date = days.day_js()
@@ -29,9 +29,9 @@ def jc_calculation(jc_value):
             current_balance = input('请输入当前账户余额（万元）：')
             if len(current_balance) == 0:
                 current_balance = 0
-            need_found = JcFormula(jc_value, current_amount, current_balance, total_days)
+            need_found = Formula(jc_value, current_amount, current_balance, total_days)
             print(
-                Fore.YELLOW + f'该客户需要存入{round(need_found.jc_amount_formula() - float(current_balance), 2)}万元，'
+                Fore.YELLOW + f'该客户需要存入{round(need_found.need_amount_formula() - float(current_balance), 2)}万元，'
                             f'方可在{end_date}成为基础户')
             jc_tool = BreakOff.cls()
         elif jc == "3":
@@ -47,8 +47,8 @@ def yx_calculation(yx_value):
         if yx == "1":
             current_amount = input('请输入当前账户日均（万元）：')
             current_balance = input('请输入当前账户余额（万元）：')
-            need_days = YxFormula(yx_value, current_amount, current_balance)
-            print(Fore.YELLOW + f'该客户将在{round(need_days.yx_need_days_formula())}天后成为基础户')
+            need_days = Formula(yx_value, current_amount, current_balance)
+            print(Fore.YELLOW + f'该客户将在{round(need_days.need_days_formula())}天后成为有效户')
             yx_tool = BreakOff.cls()
         elif yx == "2":
             total_days, end_date = days.day_js()
@@ -56,9 +56,9 @@ def yx_calculation(yx_value):
             current_balance = input('请输入当前账户余额（万元）：')
             if len(current_balance) == 0:
                 current_balance = 0
-            need_found = YxFormula(yx_value, current_amount, current_balance, total_days)
+            need_found = Formula(yx_value, current_amount, current_balance, total_days)
             print(
-                Fore.YELLOW + f'该客户需要存入{round(need_found.yx_amount_formula() - float(current_balance), 2)}万元，'
+                Fore.YELLOW + f'该客户需要存入{round(need_found.need_amount_formula() - float(current_balance), 2)}万元，'
                             f'方可在{end_date}成为有效户')
             yx_tool = BreakOff.cls()
         elif yx == "3":
